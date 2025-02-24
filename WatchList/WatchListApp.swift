b363ce9c2a1a3603818125d6ad6c1835a67327f4
Weerawut <weerawut@live.com>
2025-02-24 17:30:00 +0700

    Initial Commit

---
+++ WatchList/WatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchListApp.swift
+//  WatchList
+//
+//  Created by Weerawut Chaiyasomboon on 24/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct WatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
